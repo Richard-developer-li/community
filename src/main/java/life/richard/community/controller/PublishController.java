49f5bae3566c3673067833d715b1216a84e527f5
Richard.li <a13142139081@163.com>
2020-03-14 19:47:55 +0800

    modified code 2

--- src/main/java/life/richard/community/controller/PublishController.java
+++ src/main/java/life/richard/community/controller/PublishController.java
@@ -1,12 +1,60 @@
 package life.richard.community.controller;
 
+import life.richard.community.mapper.QuestionMapper;
+import life.richard.community.mapper.UserMapper;
+import life.richard.community.model.Question;
+import life.richard.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
     @GetMapping("/publish")
     public String publish(){
         return "publish";
     }
+
+    @PostMapping("/publish")
+    public String doPublish(@RequestParam("title") String title,
+                            @RequestParam("description") String description,
+                            @RequestParam("tag") String tag,
+                            HttpServletRequest request,
+                            Model model){
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                user = userMapper.findByToken(token);
+            }
+        }
+
+        if(user == null){
+            model.addAttribute("error","用户未登录");
+        }
+
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
+        questionMapper.create(question);
+        return "publish";
+    }
+
 }
--- src/main/java/life/richard/community/mapper/UserMapper.java
+++ src/main/java/life/richard/community/mapper/UserMapper.java
@@ -3,10 +3,16 @@ package life.richard.community.mapper;
 import life.richard.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
     @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified}) ")
     void insert(User user);
+
+    @Select("Select * from user where token = #{token} ")
+    User findByToken(@Param("token") String token);
+
 }
