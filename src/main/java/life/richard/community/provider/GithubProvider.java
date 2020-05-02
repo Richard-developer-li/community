069e85db01b4fd40a4d247fdcefebf1e74b7c298
Richard.li <a13142139081@163.com>
2020-05-02 20:06:10 +0800

    add flyway migration support

--- src/main/java/life/richard/community/provider/GithubProvider.java
+++ src/main/java/life/richard/community/provider/GithubProvider.java
@@ -15,7 +15,6 @@ public class GithubProvider {
         OkHttpClient client = new OkHttpClient();
 
         RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
-        System.out.println(accessTokenDTO);
         Request request = new Request.Builder()
                 .url("https://github.com/login/oauth/access_token")
                 .post(body)
